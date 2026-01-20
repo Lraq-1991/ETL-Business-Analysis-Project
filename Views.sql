06d6e51e74f8c6119c0ca96498515ed9f2918805
Luis Alvarez <alvarezquinteroluis@gmail.com>
2026-01-20 20:39:37 -0300

    Add files via upload

---
+++ Views.sql
@@ -0,0 +1,62 @@
+------- Views -----------------
+
+BEGIN TRANSACTION;
+GO
+
+
+CREATE OR ALTER VIEW vDimCustomers
+AS
+WITH ListOfCustomers
+AS(
+	SELECT DISTINCT CustomerID
+	FROM DimCustomerAgeData
+	UNION 
+	SELECT DISTINCT CustomerID
+	FROM DimCustomerMRRData
+	UNION
+	SELECT DISTINCT CustomerID
+	FROM DimCustomerRevenueData
+	UNION
+	SELECT DISTINCT CustomerID
+	FROM DimNewsletterInteractionData
+	UNION
+	SELECT DISTINCT CustomerID
+	FROM DimRegionAndVerticalData
+	UNION
+	SELECT DISTINCT CustomerID
+	FROM DimStatusAndLevelData
+)
+SELECT 
+	cte.CustomerID,
+	age.CustomerAgeInMonths,
+	mrr.MonthlyRecurringRevenue,
+	rev.TotalRevenue,
+	region.Region,
+	region.Subvertical,
+	region.Vertical,
+	sl.CustomerLevel,
+	sl.Status,
+	inter.CompanyNewsletterInteractionCount
+FROM ListOfCustomers cte
+LEFT OUTER JOIN DimCustomerAgeData age
+	ON cte.CustomerID = age.CustomerID
+LEFT OUTER JOIN DimCustomerMRRData mrr
+	ON cte.CustomerID = mrr.CustomerID
+LEFT OUTER JOIN DimCustomerRevenueData rev
+	ON cte.CustomerID = rev.CustomerID
+LEFT OUTER JOIN DimRegionAndVerticalData region
+	ON cte.CustomerID = region.CustomerID
+LEFT OUTER JOIN DimStatusAndLevelData sl
+	ON cte.CustomerID = sl.CustomerID
+LEFT OUTER JOIN DimNewsletterInteractionData inter
+	ON cte.CustomerID = inter.CustomerID;
+GO
+
+
+
+COMMIT TRANSACTION;
+
+
+
+
+
